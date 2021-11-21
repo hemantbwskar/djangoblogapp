7921e18c73e064532c3567bdd4ef25f65eed335d
hemantbwskar <86668867+hemantbwskar@users.noreply.github.com>
2021-11-21 07:23:56 +0530

    settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!6c%ep#ec4oe_4(47mc=m*wuwcfl*(@ee-$7ph=olcw)5!@tyb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['didleysquat.azurewebsites.net']
 
 import socket
 socket.getaddrinfo('localhost', 8090)
